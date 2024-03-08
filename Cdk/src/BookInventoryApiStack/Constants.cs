9d5c71ff64da22dab5c9d3315c69d1f57d2b1320
Vedanayagan Damodaran <vedanayagan.damodaran@bp.com>
2024-03-07 20:03:58 -0600

    Updated metrics

--- Cdk/src/BookInventoryApiStack/Constants.cs
+++ Cdk/src/BookInventoryApiStack/Constants.cs
@@ -3,9 +3,9 @@
     internal static class Constants
     {
         public const string SERVICE_NAME = "BookInventory";
-        public const string ADD_BOOK_API = "Add_Book";
-        public const string SEARCH_BOOK_API = "Search_Book";
-        public const string LIST_BOOK_API = "List_Book";
-        public const string UPDATE_BOOK_API = "Update_Book";
+        public const string ADD_BOOK_API = "AddBook";
+        public const string SEARCH_BOOK_API = "SearchBook";
+        public const string LIST_BOOK_API = "ListBook";
+        public const string UPDATE_BOOK_API = "UpdateBook";
     }
 }
